89de67bf8805f6fc3a67761693b8f0aa86d403db
Anthony Yos <anthonyvyos@gmail.com>
2022-08-09 13:53:51 -0500

    Add form component to AddCharacter(not final)

--- client/src/components/Character/AddCharacter.js
+++ client/src/components/Character/AddCharacter.js
@@ -1,50 +1,63 @@
-import React, { useState, useContext } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { CharacterContext } from '../../context/CharacterContext';
-import CharacterType from '../../context/CharacterType';
+import ApiEndpoint from '../../context/ResourceType';
+import { add } from '../../util/functions/add';
+import Form from '../Form/Form';
+import Input from '../Form/Input';
+import Select from '../Form/Select';
+import Button from '../UI/Button';
+import { alignmentOptions } from '../../util/alignmentOptions';
 
-export default function AddCharacter({ characterType }) {
-  const [name, setName] = useState('');
-  const { addCharacter } = useContext(CharacterContext);
+export default function AddCharacter({ resourceType }) {
   const navigate = useNavigate();
 
+  const btnLabel = resourceType === ApiEndpoint.PLAYER ? 'Add Player' : 'Add Enemy';
+  const characterType = resourceType === ApiEndpoint.PLAYER ? 'player' : 'enemy';
+
   const onSubmit = e => {
-    e.preventDefault();
     const newCharacter = {
-      name,
+      ...e,
+      type: characterType,
       speed: 50,
       stats: { str: 5, dex: 5, con: 5, int: 5, wis: 5, cha: 5 },
-      alignment: 'Neutral',
-      race: 'Humanoid',
       armorClass: 20,
-      health: 50,
     };
-
-    addCharacter(newCharacter, characterType);
-    switch (characterType) {
-      case CharacterType.PLAYER:
-        return navigate('/players');
-      case CharacterType.ENEMY:
-        return navigate('/enemies');
-      default:
-        return navigate('/');
+    try {
+      console.log(newCharacter);
+      // add(newCharacter, resourceType);
+      // switch (resourceType) {
+      //   case ApiEndpoint.PLAYER:
+      //     return navigate('/players');
+      //   case ApiEndpoint.ENEMY:
+      //     return navigate('/enemies');
+      //   default:
+      //     return navigate('/');
+      // }
+    } catch (error) {
+      console.log(error, 'errrororor');
     }
   };
 
   return (
     <React.Fragment>
-      <form className='col-lg-6 offset-lg-3 card' onSubmit={onSubmit}>
-        <div className='form-control'>
-          <label htmlFor='text'>Name</label>
-          <input
-            type='text'
-            value={name}
-            onChange={e => setName(e.target.value)}
-            placeholder='Enter Name...'
-          />
+      <section className='row'>
+        <div className='col-sm-4 offset-sm-4 card'>
+          <Form onSubmit={onSubmit}>
+            <Input name='name' label='Name' className='form-group col-5 offset-4' />
+            <Input name='race' label='Race' />
+            <Input name='health' label='Health' type='number' min={1} max={500} />
+            <Select
+              name='alignment'
+              label='Alignment'
+              className='form-group col-5 offset-4'
+              options={alignmentOptions}
+            />
+            <Button className='btn btn-success offset-5 col-2' type='submit'>
+              {btnLabel}
+            </Button>
+          </Form>
         </div>
-        <button className='btn btn-success'>Add Enemy</button>
-      </form>
+      </section>
     </React.Fragment>
   );
 }

004beb2529071e21b4d124d3a2a400b79380b98e
Anthony Yos <anthonyvyos@gmail.com>
2022-08-09 13:11:05 -0500

    Remove CharacterProvider from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CharacterProvider } from './context/CharacterContext';
 import { CharacterList } from './components/Character/CharacterList';
 import AddCharacter from './components/Character/AddCharacter';
 import Navbar from './components/UI/Navbar';
@@ -11,29 +10,24 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <CharacterProvider>
-        <Routes>
-          <Route path='/' element={<h1>Homepage</h1>} />
-          <Route
-            path='/create/enemies'
-            element={<AddCharacter resourceType={ApiEndPoint.ENEMY} />}
-          />
-          <Route path='/enemies' element={<CharacterList resourceType={ApiEndPoint.ENEMY} />} />
-          <Route
-            path='/enemies/:id'
-            element={<CharacterDetails resourceType={ApiEndPoint.ENEMY} />}
-          />
-          <Route
-            path='/create/players'
-            element={<AddCharacter resourceType={ApiEndPoint.PLAYER} />}
-          />
-          <Route path='/players' element={<CharacterList resourceType={ApiEndPoint.PLAYER} />} />
-          <Route
-            path='/players/:id'
-            element={<CharacterDetails resourceType={ApiEndPoint.PLAYER} />}
-          />
-        </Routes>
-      </CharacterProvider>
+      <Routes>
+        <Route path='/' element={<h1>Homepage</h1>} />
+        <Route path='/create/enemies' element={<AddCharacter resourceType={ApiEndPoint.ENEMY} />} />
+        <Route path='/enemies' element={<CharacterList resourceType={ApiEndPoint.ENEMY} />} />
+        <Route
+          path='/enemies/:id'
+          element={<CharacterDetails resourceType={ApiEndPoint.ENEMY} />}
+        />
+        <Route
+          path='/create/players'
+          element={<AddCharacter resourceType={ApiEndPoint.PLAYER} />}
+        />
+        <Route path='/players' element={<CharacterList resourceType={ApiEndPoint.PLAYER} />} />
+        <Route
+          path='/players/:id'
+          element={<CharacterDetails resourceType={ApiEndPoint.PLAYER} />}
+        />
+      </Routes>
     </BrowserRouter>
   );
 }

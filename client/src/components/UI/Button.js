863ada12b37fb45cd16b2b8cd4a97a90827f13c4
Anthony Yos <anthonyvyos@gmail.com>
2022-08-09 13:17:06 -0500

    Add type property to Button component

--- client/src/components/UI/Button.js
+++ client/src/components/UI/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function Button({ className, onClick, children }) {
+export default function Button({ className, onClick, type, children }) {
   return (
-    <button className={className} onClick={onClick}>
+    <button className={className} type={type || 'button'} onClick={onClick}>
       {children}
     </button>
   );

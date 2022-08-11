5882672dc4689567905c31a99cfeca48feb6aa03
Anthony Yos <anthonyvyos@gmail.com>
2022-08-11 13:20:34 -0500

    Form comp  prefills stat & modifier fields

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function Form({ formData, children, onSubmit }) {
+  const { stats, ...data } = formData; // eventually destructure out modifiers object and spread it into defaultValues
+  const defaultValues = { ...data, ...stats, cha_mod: 5 };
+  console.log(defaultValues);
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ defaultValues: formData });
+  } = useForm({ defaultValues });
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>

60d8e974821a1e4313fe77bc101d998d9ba18563
Anthony Yos <anthonyvyos@gmail.com>
2022-08-11 13:28:28 -0500

    Remove unused imports

--- client/src/components/Character/CharacterList.js
+++ client/src/components/Character/CharacterList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import CharacterCard from './CharacterCard/CharacterCard';
 import useFetch from '../../hooks/useFetch';
 

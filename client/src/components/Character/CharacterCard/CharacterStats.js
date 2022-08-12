28aa97261bfce4b29c9f0b69725a431d7f350c24
Anthony Yos <anthonyvyos@gmail.com>
2022-08-12 12:45:45 -0500

    CharacterStat uses/displays modifier values.

--- client/src/components/Character/CharacterCard/CharacterStats.js
+++ client/src/components/Character/CharacterCard/CharacterStats.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import Stat from './Stat';
 
-export default function CharacterStats({ stats, className }) {
+export default function CharacterStats({ stats, modifiers, className }) {
   return (
     <React.Fragment>
-      <p className={className}>
-        <Stat label='Str' statValue={stats.str} modifier={'placeholder'} />
+      <div className={className}>
+        <Stat label='Str' statValue={stats.str} modifier={modifiers.str_mod} />
         <br />
-        <Stat label='Dex' statValue={stats.dex} modifier={'placeholder'} />
+        <Stat label='Dex' statValue={stats.dex} modifier={modifiers.dex_mod} />
         <br />
-        <Stat label='Con' statValue={stats.con} modifier={'placeholder'} />
+        <Stat label='Con' statValue={stats.con} modifier={modifiers.con_mod} />
         <br />
-      </p>
-      <p className={className}>
-        <Stat label='Int' statValue={stats.int} modifier={'placeholder'} />
+      </div>
+      <div className={className}>
+        <Stat label='Int' statValue={stats.int} modifier={modifiers.int_mod} />
         <br />
-        <Stat label='Wis' statValue={stats.wis} modifier={'placeholder'} />
+        <Stat label='Wis' statValue={stats.wis} modifier={modifiers.wis_mod} />
         <br />
-        <Stat label='Cha' statValue={stats.cha} modifier={'placeholder'} />
+        <Stat label='Cha' statValue={stats.cha} modifier={modifiers.cha_mod} />
         <br />
-      </p>
+      </div>
     </React.Fragment>
   );
 }
--- client/src/components/Character/CharacterCard/Stat.js
+++ client/src/components/Character/CharacterCard/Stat.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export default function Stat({ label, statValue, modifier }) {
+  const modifierSign = modifier >= 0 ? '+' : '-';
   return (
     <React.Fragment>
       <strong>{label}: </strong>
-      {statValue}&ensp;{modifier}
+      {statValue}&ensp;{modifierSign + Math.abs(modifier)}
     </React.Fragment>
   );
 }

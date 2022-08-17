0608351fb86e9ca7c3398699b98d65eeddea5868
Anthony Yos <anthonyvyos@gmail.com>
2022-08-17 13:46:58 -0500

    Edit comment placement to correct place

--- util/Errors/errorHandler.js
+++ util/Errors/errorHandler.js
@@ -1,8 +1,8 @@
 module.exports = function errorHandler(err, req, res, next) {
-  // statusCode defaults to 500 if statusCode is outside the 4xx/5xx range
   if (res.headersSent) {
     return next(err);
   }
+  // status defaults to 500 if status is outside the 4xx/5xx range
   const { status } = err;
   if (!err.message) err.message = 'Something went bad';
   res.status(status).json({ success: false, error: err.message });

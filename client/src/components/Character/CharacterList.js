5daec84ff33a517119e7f1040d57fe4f9dee366c
Anthony Yos <anthonyvyos@gmail.com>
2022-08-19 13:42:54 -0500

    Create search fields, states, comments
    Create a search url used by useFetch with search comps and new states.
    
    Begin adding comments

--- client/src/components/Character/CharacterList.js
+++ client/src/components/Character/CharacterList.js
@@ -4,63 +4,92 @@ import useFetch from '../../hooks/useFetch';
 import ApiEndpoint from '../../context/ResourceType';
 import { Link } from 'react-router-dom';
 import LoadingSpinner from '../UI/LoadingSpinner';
+import SearchInput from '../Search/SearchInput';
+import SearchSelect from '../Search/SearchSelect';
+import { searchOptions } from '../../util/searchOptions';
 
 export const CharacterList = ({ resourceType }) => {
   const apiUrl = `/api/v1/${resourceType}`;
   const [url, setUrl] = useState(apiUrl);
-  const [search, setSearch] = useState('');
-
-  // useEffect(() => {
-  //   setUrl(`/api/v1/${resourceType}`);
-  // }, [resourceType]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchFilter, setSearchFilter] = useState('name');
 
   const { data: characters, isPending, error, setData: setCharacters } = useFetch(url);
 
-  useEffect(() => setSearch(''), [apiUrl]);
+  // Resets search field when switching between CharacterList for players & enemies
+  useEffect(() => setSearchTerm(''), [apiUrl]);
 
+  // Sets a new url to be useed by useFetch when searchTerm changes
   useEffect(() => {
     const searchLookup = setTimeout(() => {
-      if (search) {
-        setUrl(apiUrl + `?query1=${search}`);
-      } else setUrl(apiUrl);
+      if (searchTerm) setUrl(apiUrl + `?filter=${searchFilter}&query=${searchTerm}`);
+      else setUrl(apiUrl);
     }, 500);
     return () => {
       clearTimeout(searchLookup);
     };
-  }, [search, apiUrl]);
+  }, [searchTerm, apiUrl]);
 
-  const handleSearch = e => {
-    setSearch(e.target.value);
+  const handleSearchTerm = e => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearchFilter = e => {
+    setSearchFilter(e.target.value.toLowerCase());
   };
 
   const addCharacterLabel = resourceType === ApiEndpoint.PLAYER ? 'Add Player' : 'Add Enemy';
   const characterType = resourceType === ApiEndpoint.PLAYER ? 'Player(s)' : 'Enemy / Enemies';
 
   return (
-    <section className='row m-3'>
-      <input type='text' onChange={handleSearch} value={search} />
-      {isPending && <LoadingSpinner />}
-      {error && <div>{error}</div>}
-      {characters && !characters.length && (
-        <React.Fragment>
-          <h3 className='text-center mb-3'>No {`${characterType}`} were found</h3>
-          <Link
-            className='text-center btn btn-success col-md-2 offset-md-5 col-4 offset-4'
-            to={`/create/${resourceType}`}>
-            {addCharacterLabel}
-          </Link>
-        </React.Fragment>
-      )}
-      {characters &&
-        characters.length > 0 &&
-        characters.map(character => (
-          <CharacterCard
-            key={character._id}
-            character={character}
-            resourceType={resourceType}
-            characterListState={{ characters, setCharacters }}
+    <React.Fragment>
+      <section className='row mt-3'>
+        <div className='offset-4'>
+          <SearchSelect
+            name='filter'
+            label='Filter'
+            className='m-2'
+            options={searchOptions}
+            onChange={handleSearchFilter}
           />
-        ))}
-    </section>
+          <SearchInput
+            name='search'
+            type='text'
+            label='Search'
+            placeholder='Search...'
+            value={searchTerm}
+            className='m-2'
+            onChange={handleSearchTerm}
+          />
+          {/* <input type='text' onChange={handleSearch} value={search} /> */}
+        </div>
+      </section>
+      <section className='row m-3'>
+        {/* Display loading spinner, error, or button; when a request is made for data, an error occurs, or receive a response where there is no data*/}
+        {isPending && <LoadingSpinner />}
+        {error && <div>{error}</div>}
+        {characters && !characters.length && (
+          <React.Fragment>
+            <h3 className='text-center mb-3'>No {`${characterType}`} were found</h3>
+            <Link
+              className='text-center btn btn-success col-md-2 offset-md-5 col-4 offset-4'
+              to={`/create/${resourceType}`}>
+              {addCharacterLabel}
+            </Link>
+          </React.Fragment>
+        )}
+        {/* If there is characters data, display them in the form of CharacterCard */}
+        {characters &&
+          characters.length > 0 &&
+          characters.map(character => (
+            <CharacterCard
+              key={character._id}
+              character={character}
+              resourceType={resourceType}
+              characterListState={{ characters, setCharacters }}
+            />
+          ))}
+      </section>
+    </React.Fragment>
   );
 };

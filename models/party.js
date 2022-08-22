b356c06ec315e17f24809bf332ba242a32b2c2e1
Anthony Yos <anthonyvyos@gmail.com>
2022-08-22 15:01:29 -0500

    Create party model, allow case insenitive searches
    Add collation for case insensitive search to   character & party models

--- models/party.js
+++ models/party.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
 
-const partySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  members: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Character',
+const partySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-  ],
-});
+    type: {
+      type: String,
+      required: true,
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Character',
+      },
+    ],
+  },
+  { collation: { locale: 'en_US', strength: 2 } }
+);
 
 module.exports = mongoose.model('Party', partySchema);

3e3f7f48b095518648f181422a4836d703bd409d
Anthony Yos <anthonyvyos@gmail.com>
2022-09-15 12:41:04 -0500

    Refactor useFetch to create axiosFetch function

---
+++ client/src/hooks/useAxiosFunction.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+export default function useAxiosFunction() {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [controller, setController] = useState();
+
+  const axiosFetch = async configObj => {
+    const { axiosInstance, method, url, requestConfig = {} } = configObj;
+    try {
+      setLoading(true);
+      setResponse(null);
+      setError(null);
+      const ctrl = new AbortController();
+      setController(ctrl);
+      const res = await axiosInstance({
+        url,
+        method: method.toLowerCase(),
+        ...requestConfig,
+        signal: ctrl.signal,
+      });
+      setResponse(res.data.data);
+    } catch (error) {
+      setError(error);
+      if (error.name !== 'CanceledError') {
+        setError(error.response.data.error);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    return () => controller && controller.abort();
+  }, [controller]);
+
+  return { response, loading, error, setResponse, axiosFetch };
+}

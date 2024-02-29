494e4f2208ee20bc09eabf4a758fdd05971205a3
Anthony Yos <anthonyvyos@gmail.com>
2024-02-29 16:45:22 -0600

    Change DB uri

--- Backend/db/database.js
+++ Backend/db/database.js
@@ -3,9 +3,7 @@ const dotenv = require('dotenv').config({ path: './config/config.env' });
 const DB_URI = process.env.DB_URI;
 
 const connect = async () => {
-  await mongoose.connect(
-    'mongodb+srv://classydonut:K!ngDonut95@cluster0.ufg6t.mongodb.net/dnd?retryWrites=true&w=majority'
-  );
+  await mongoose.connect(DB_URI);
   console.log('MongoDB connected.');
 };
 

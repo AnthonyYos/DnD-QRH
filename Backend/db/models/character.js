276335c24a3e4642b70d3e5a4e141f1b53d46a91
Anthony Yos <anthonyvyos@gmail.com>
2024-03-07 12:04:12 -0600

    Add more properties to character model

--- Backend/db/models/character.js
+++ Backend/db/models/character.js
@@ -2,50 +2,71 @@ const mongoose = require('mongoose');
 
 const characterSchema = new mongoose.Schema({
   type: { type: String, required: true },
+
   name: {
     type: String,
     required: true,
   },
+
   armorClass: {
-    type: Number,
+    type: String,
     required: true,
   },
+
   health: {
-    type: Number,
+    type: String,
     required: true,
   },
+
   speed: {
     type: String,
     required: true,
   },
-  alignment: {
-    type: String,
-    required: true,
-  },
+
+  saving_throws: String,
+
+  skills: String,
+
+  senses: String,
+
+  languages: String,
+
+  traits: String,
+
+  actions: String,
+
+  legendary_actions: String,
+
   meta: {
     type: String,
     required: true,
   },
+
   str: {
     type: Number,
     required: true,
   },
+
   dex: {
     type: Number,
     required: true,
   },
+
   con: {
     type: Number,
     required: true,
   },
+
   int: {
     type: Number,
     required: true,
   },
+
   wis: {
     type: Number,
     required: true,
   },
+
   cha: {
     type: Number,
     required: true,
@@ -55,22 +76,27 @@ const characterSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+
   dex_mod: {
     type: Number,
     required: true,
   },
+
   con_mod: {
     type: Number,
     required: true,
   },
+
   int_mod: {
     type: Number,
     required: true,
   },
+
   wis_mod: {
     type: Number,
     required: true,
   },
+
   cha_mod: {
     type: Number,
     required: true,
